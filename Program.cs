777a0be1fdb4e5ce7eba05a64006b518cc765295
soniadila <99525698+soniadila@users.noreply.github.com>
2022-02-27 07:50:35 +0700

    projek kedua

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 26/02/2022
+ * Time: 11:00
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace BelajarCSHARP2
+{
+	class ProgramHW
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Nama kamu siapa?");
+			
+			string nama = Console.ReadLine();
+			Console.WriteLine("Sonia Dila Puspita"+nama);
+			
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
